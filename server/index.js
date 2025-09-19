3abcd8c2c2d9672d4dc50008822a7dddc0a3397d
Daksh <203952304+Daksh-chhabra@users.noreply.github.com>
2025-09-19 14:08:07 +0530

    Use environment variable for server port

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@
 
 
     const app = express();
-    const PORT = 5000;
+    const PORT = process.env.PORT || 5000;
 
     app.use((req, res, next) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
